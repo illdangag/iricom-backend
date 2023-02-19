8613c2a38218f8e2e7da0fd55f7d5df6c1989659
illdangag <illdangag@gmail.com>
2023-02-19 16:33:29 +0900

    CORS 설정

--- src/main/java/com/illdangag/iricom/server/configuration/config/WebMvcConfig.java
+++ src/main/java/com/illdangag/iricom/server/configuration/config/WebMvcConfig.java
@@ -5,6 +5,7 @@ import com.illdangag.iricom.server.configuration.resolver.RequestContextResolver
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -29,5 +30,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(this.requestContextResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*");
     }
 }
