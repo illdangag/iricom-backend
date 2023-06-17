3dfc1d57174eace00a08cfafbb7584b2940c1b1e
illdangag <illdangag@gmail.com>
2023-06-17 17:33:50 +0900

    신고 내역 조회 API

--- src/main/java/com/illdangag/iricom/server/controller/v1/ReportController.java
+++ src/main/java/com/illdangag/iricom/server/controller/v1/ReportController.java
@@ -6,9 +6,11 @@ import com.illdangag.iricom.server.configuration.annotation.AuthRole;
 import com.illdangag.iricom.server.configuration.annotation.RequestContext;
 import com.illdangag.iricom.server.data.entity.Account;
 import com.illdangag.iricom.server.data.request.CommentReportCreate;
+import com.illdangag.iricom.server.data.request.CommentReportInfoSearch;
 import com.illdangag.iricom.server.data.request.PostReportInfoCreate;
 import com.illdangag.iricom.server.data.request.PostReportInfoSearch;
 import com.illdangag.iricom.server.data.response.CommentReportInfo;
+import com.illdangag.iricom.server.data.response.CommentReportInfoList;
 import com.illdangag.iricom.server.data.response.PostReportInfo;
 import com.illdangag.iricom.server.data.response.PostReportInfoList;
 import com.illdangag.iricom.server.service.ReportService;
@@ -38,6 +40,18 @@ public class ReportController {
                                                                     @PathVariable(value = "post_id") String postId,
                                                                     @RequestBody @Valid PostReportInfoSearch postReportInfoSearch,
                                                                     @RequestContext Account account) {
+        // TODO
+        return null;
+    }
+
+    @ApiCallLog(apiCode = "RP_002")
+    @Auth(role = AuthRole.BOARD_ADMIN)
+    @RequestMapping(method = RequestMethod.GET, value = "/v1/boards/{board_id}/posts/{post_id}/report/{report_id}")
+    public ResponseEntity<PostReportInfo> getPostReportInfo(@PathVariable(value = "board_id") String boardId,
+                                                            @PathVariable(value = "post_id") String postId,
+                                                            @PathVariable(value = "report_id") String reportId,
+                                                            @RequestContext Account account) {
+        // TODO
         return null;
     }
 
@@ -50,6 +64,31 @@ public class ReportController {
                                                      @RequestContext Account account) {
         PostReportInfo postReportInfo = this.reportService.reportPost(account, boardId, postId, postReportInfoCreate);
         return ResponseEntity.status(HttpStatus.OK).body(postReportInfo);
+    }
+
+    @ApiCallLog(apiCode = "RC_001")
+    @Auth(role = AuthRole.BOARD_ADMIN)
+    @RequestMapping(method = RequestMethod.GET, value = "/v1/boards/{board_id}/posts/{post_id}/comments/{comment_id}/report")
+    public ResponseEntity<CommentReportInfoList> getCommentReportInfoList(@PathVariable(value = "board_id") String boardId,
+                                                                          @PathVariable(value = "post_id") String postId,
+                                                                          @PathVariable(value = "comment_id") String commentId,
+                                                                          @RequestBody @Valid CommentReportInfoSearch commentReportInfoSearch,
+                                                                          @RequestContext Account account) {
+        // TODO
+        return null;
+    }
+
+    @ApiCallLog(apiCode = "RC_002")
+    @Auth(role = AuthRole.BOARD_ADMIN)
+    @RequestMapping(method = RequestMethod.GET, value = "/v1/boards/{board_id}/posts/{post_id}/comments/{comment_id}/report/{report_id}")
+    public ResponseEntity<CommentReportInfo> getCommentReportInfo(@PathVariable(value = "board_id") String boardId,
+                                                               @PathVariable(value = "post_id") String postId,
+                                                               @PathVariable(value = "comment_id") String commentId,
+                                                               @PathVariable(value = "report_id") String reportId,
+                                                               @RequestBody @Valid PostReportInfoSearch postReportInfoSearch,
+                                                               @RequestContext Account account) {
+        // TODO
+        return null;
     }
 
     @ApiCallLog(apiCode = "RC_003")
---
+++ src/main/java/com/illdangag/iricom/server/data/request/CommentReportInfoSearch.java
@@ -0,0 +1,19 @@
+package com.illdangag.iricom.server.data.request;
+
+import com.illdangag.iricom.server.data.entity.ReportType;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@ToString
+public class CommentReportInfoSearch extends SearchRequest {
+    private ReportType type;
+
+    @Builder.Default
+    private String reason = "";
+}
