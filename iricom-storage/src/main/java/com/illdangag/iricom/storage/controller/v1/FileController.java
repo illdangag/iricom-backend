d5bdb20a6155a0bc7e311fa2d9306b4d6881a9b6
illdangag <illdangag@gmail.com>
2023-08-12 16:57:22 +0900

    storage module

---
+++ iricom-storage/src/main/java/com/illdangag/iricom/storage/controller/v1/FileController.java
@@ -0,0 +1,24 @@
+package com.illdangag.iricom.storage.controller.v1;
+
+import com.illdangag.iricom.service.configuration.annotation.ApiCallLog;
+import com.illdangag.iricom.service.configuration.annotation.Auth;
+import com.illdangag.iricom.service.configuration.annotation.AuthRole;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/v1/file")
+public class FileController {
+
+    @ApiCallLog(apiCode = "FL_001")
+    @Auth(role = { AuthRole.NONE, })
+    @RequestMapping(method = RequestMethod.GET, value = "")
+    public ResponseEntity<String> requestTest() {
+
+        return ResponseEntity.status(200).body("FL_001");
+    }
+}
