31a18b8eb20fab27eec15bef0e1e112470334857
illdangag <illdangag@gmail.com>
2025-02-19 23:18:40 +0900

    기본 정보 API 단위 테스트 개선

--- iricom-server/src/test/java/com/illdangag/iricom/server/controller/v1/InformationControllerTest.java
+++ iricom-server/src/test/java/com/illdangag/iricom/server/controller/v1/InformationControllerTest.java
@@ -1,7 +1,7 @@
 package com.illdangag.iricom.server.controller.v1;
 
-import com.illdangag.iricom.server.data.response.AccountInfo;
 import com.illdangag.iricom.server.test.IricomTestSuite;
+import com.illdangag.iricom.server.test.data.wrapper.TestAccountInfo;
 import com.illdangag.iricom.server.test.data.wrapper.TestBoardInfo;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -13,6 +13,7 @@ import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilde
 
 import java.util.Collections;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -31,48 +32,49 @@ public class InformationControllerTest extends IricomTestSuite {
     @Nested
     @DisplayName("기본 정보")
     class DefaultInfoGet {
-
         @Test
         @DisplayName("관리자 계정")
-        public void testCase00() throws Exception {
-            AccountInfo accountInfo = getAccount(systemAdmin);
-
+        void testCase00() throws Exception {
             MockHttpServletRequestBuilder requestBuilder = get("/v1/infos");
             setAuthToken(requestBuilder, systemAdmin);
 
             mockMvc.perform(requestBuilder)
                     .andExpect(status().is(200))
-                    .andExpect(jsonPath("$.email").value(accountInfo.getEmail()))
+                    .andExpect(jsonPath("$.email").value(systemAdmin.getEmail()))
                     .andExpect(jsonPath("$.auth").value("systemAdmin"))
                     .andDo(print());
         }
 
         @Test
         @DisplayName("게시판 관리자 계정")
-        public void testCase01() throws Exception {
-            AccountInfo accountInfo = getAccount(allBoardAdmin);
+        void testCase01() throws Exception {
+            // 계정 생성
+            TestAccountInfo account = setRandomAccount();
+            // 게시판 생성
+            setRandomBoard(Collections.singletonList(account));
 
             MockHttpServletRequestBuilder requestBuilder = get("/v1/infos");
-            setAuthToken(requestBuilder, allBoardAdmin);
+            setAuthToken(requestBuilder, account);
 
             mockMvc.perform(requestBuilder)
                     .andExpect(status().is(200))
-                    .andExpect(jsonPath("$.email").value(accountInfo.getEmail()))
+                    .andExpect(jsonPath("$.email").value(account.getEmail()))
                     .andExpect(jsonPath("$.auth").value("boardAdmin"))
                     .andDo(print());
         }
 
         @Test
         @DisplayName("일반 계정")
-        public void testCase02() throws Exception {
-            AccountInfo accountInfo = getAccount(common00);
+        void testCase02() throws Exception {
+            // 계정 생성
+            TestAccountInfo account = setRandomAccount();
 
             MockHttpServletRequestBuilder requestBuilder = get("/v1/infos");
-            setAuthToken(requestBuilder, common00);
+            setAuthToken(requestBuilder, account);
 
             mockMvc.perform(requestBuilder)
                     .andExpect(status().is(200))
-                    .andExpect(jsonPath("$.email").value(accountInfo.getEmail()))
+                    .andExpect(jsonPath("$.email").value(account.getEmail()))
                     .andDo(print());
         }
     }
@@ -81,16 +83,23 @@ public class InformationControllerTest extends IricomTestSuite {
     @DisplayName("작성한 게시물 목록")
     class GetPostList {
         @Test
-        public void testCase00() throws Exception {
+        void testCase00() throws Exception {
+            // 계정 생성
+            TestAccountInfo account = setRandomAccount();
+            // 게시판 생성
+            TestBoardInfo board = setRandomBoard();
+            // 게시물 생성
+            setRandomPost(board, account, 13);
+
             MockHttpServletRequestBuilder requestBuilder = get("/v1/infos/posts");
-            setAuthToken(requestBuilder, common00);
+            setAuthToken(requestBuilder, account);
 
             mockMvc.perform(requestBuilder)
                     .andExpect(status().is(200))
-                    .andExpect(jsonPath("$.total").exists())
-                    .andExpect(jsonPath("$.skip").exists())
-                    .andExpect(jsonPath("$.limit").exists())
-                    .andExpect(jsonPath("$.posts").isArray())
+                    .andExpect(jsonPath("$.total").value(13))
+                    .andExpect(jsonPath("$.skip").value(0))
+                    .andExpect(jsonPath("$.limit").value(20))
+                    .andExpect(jsonPath("$.posts", hasSize(13)))
                     .andDo(print());
         }
     }
@@ -100,34 +109,22 @@ public class InformationControllerTest extends IricomTestSuite {
     class BoardAdminGetByBoardAdminTest {
         @Test
         @DisplayName("목록 조회")
-        public void getList() throws Exception {
-            // 게시판
-            TestBoardInfo testBoardInfo00 = TestBoardInfo.builder()
-                    .title("testBoardInfo00").isEnabled(true).undisclosed(false)
-                    .adminList(Collections.singletonList(common00))
-                    .build();
-            TestBoardInfo testBoardInfo01 = TestBoardInfo.builder()
-                    .title("testBoardInfo01").isEnabled(true).undisclosed(false)
-                    .adminList(Collections.singletonList(common00))
-                    .build();
-            TestBoardInfo testBoardInfo02 = TestBoardInfo.builder()
-                    .title("testBoardInfo02").isEnabled(true).undisclosed(false)
-                    .adminList(Collections.singletonList(common00))
-                    .build();
-            addTestBoardInfo(testBoardInfo00, testBoardInfo01, testBoardInfo02);
-            init();
+        void getList() throws Exception {
+            // 계정 생성
+            TestAccountInfo account = setRandomAccount();
+            // 게시판 생성
+            setRandomBoard(Collections.singletonList(account), 5);
 
             MockHttpServletRequestBuilder requestBuilder = get("/v1/infos/admin/boards");
-            setAuthToken(requestBuilder, common00);
+            setAuthToken(requestBuilder, account);
 
             mockMvc.perform(requestBuilder)
                     .andExpect(status().is(200))
-                    .andExpect(jsonPath("$.total").exists())
+                    .andExpect(jsonPath("$.total").value(5))
                     .andExpect(jsonPath("$.skip").value(0))
                     .andExpect(jsonPath("$.limit").value(20))
-                    .andExpect(jsonPath("$.boards").isArray())
+                    .andExpect(jsonPath("$.boards", hasSize(5)))
                     .andDo(print());
         }
-
     }
 }
